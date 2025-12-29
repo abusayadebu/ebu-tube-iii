1164f543d9236a8ef52ec6422215621c2725c312
abusayadebu <abusayad743@gmail.com>
2025-12-29 16:44:01 +0600

    categories data load done

--- js/video.js
+++ js/video.js
@@ -1,1 +1,20 @@
-console.log("bvideo file connected")
+// Fetch, Load and show the categories on html--
+
+// create loadCategories
+
+const loadCategories = () => {
+    // fetch the data
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+    
+}
+
+// create displayCategories
+const displayCategories = (data) => {
+    console.log(data);
+}
+
+loadCategories();
+// displayCategories();
